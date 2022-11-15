57a1f117e3469fe512743353ab82c0bfe5dd814e
JoXW100 <joxw100@gmail.com>
2022-11-15 12:26:34 +0100

    Exposed CellState in package

--- src/structure/__init__.py
+++ src/structure/__init__.py
@@ -1,7 +1,7 @@
 from .action import Action
 from .stack import Stack
 from .piece import Piece
-from .board_cell import BoardCell
+from .board_cell import BoardCell, CellState
 from .board import Board
 from .color import Color
 from .game_data import GameData
