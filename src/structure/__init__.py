f0e62219de0422478d0358fb69aef62f302c8eaa
Jakob Wallén <57936645+JoXW100@users.noreply.github.com>
2022-11-18 11:10:20 +0100

    Implemented mine distribution, revealing mines, iproved rendering of board, and changed how locations are rendered (#13)

--- src/structure/__init__.py
+++ src/structure/__init__.py
@@ -1,7 +1,5 @@
 from .action import Action
-from .stack import Stack
-from .piece import Piece
-from .board_cell import BoardCell, CellState
+from .board_cell import BoardCell, CellState 
 from .board import Board
 from .color import Color
 from .game_data import GameData
--- src/structure/board.py
+++ src/structure/board.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+from typing import Callable
+from collections import deque
+from random import randint
 import structure as st
 
 class Board:
@@ -17,7 +20,8 @@ class Board:
         assert(size > 0)
         self.__size = size
         self.__rows = [
-            [ st.BoardCell() for x in range(size)] for y in range(size)
+            [ st.BoardCell(st.Location(x,y), self) for x in range(size)
+            ] for y in range(size)
         ]
     
     @property
@@ -48,30 +52,145 @@ class Board:
             return None
         return self.rows[loc.y][loc.x]
     
-    def get_adjacent(self, loc: st.Location, filter = lambda x: True) -> list[st.Location]:
+    def get_neighbors(self, loc: st.Location, filter: Callable[[st.BoardCell], bool] = lambda x: True) -> list[st.BoardCell]:
         """
-        Returns all valid adjacent locations to the given location that pass
+        Returns all valid neighboring cells (8) to the given location that pass
         the given filter 
         
         Attributes
         ----------
-        loc : location.Location
-            the location to check
-        filter : function
+        loc : Location
+            the location to find surrounding tiles
+        filter : callable(BoardCell) -> Bool
             the filter each location must pass 
         Returns
         -------
-        list[Location]
-            A list of adjacent locations that pass the given filter
-        """
-        return [ l for l in 
+        list[BoardCell]
+            A list of neighboring cells that pass the given filter
+        """  
+        return [ self.rows[l.y][l.x] for l in 
             [ st.Location(x, y) 
                 for x in range(loc.x - 1, loc.x + 2) 
                 for y in range(loc.y - 1, loc.y + 2) 
-            ] if l != loc and l.validate(self.size) and filter(l)
+            ] if l != loc 
+            and l.validate(self.size) 
+            and filter(self.rows[l.y][l.x])
         ]
+        
+    def get_adjacent(self, loc: st.Location, filter: Callable[[st.BoardCell], bool] = lambda x: True) -> list[st.BoardCell]:
+        """
+        Returns all valid adjacent cells (4) to the given location that pass
+        the given filter 
+        
+        Attributes
+        ----------
+        loc : Location
+            the location to find surrounding tiles
+        filter : callable(BoardCell) -> Bool
+            the filter each location must pass 
+        Returns
+        -------
+        list[BoardCell]
+            A list of adjacent cells that pass the given filter
+        """
+        a = st.Location(loc.x, loc.y + 1) # Above
+        b = st.Location(loc.x, loc.y - 1) # Below
+        r = st.Location(loc.x + 1, loc.y) # Right
+        l = st.Location(loc.x - 1, loc.y) # Left
+        # Return all locations that are on the board, and pass the given filter 
+        return [self.rows[l.y][l.x] for l in [a,b,r,l] if l.validate(self.size) 
+                and filter(self.rows[l.y][l.x])] 
     
-    def __eq__(self, other) -> bool:
+    
+    def get_all_cells(self) -> list[st.BoardCell]:
+        """
+        Creates a list of all the locations on the board
+        
+        Returns
+        -------
+        locs : list[Location]
+            A list of all the locations on the board
+        """
+        return [self.rows[y][x] for x in range(self.size) for y in range(self.size)]
+
+    def __inner_reveal(self, cell: st.BoardCell):
+        visited = {cell.location}
+        stack = deque([cell])
+        while len(stack) > 0:
+            cell = stack.pop()
+            cell.set_state(st.CellState.Visible)
+            for adj in self.get_adjacent(cell.location):
+                if adj.location not in visited:
+                    visited.add(adj.location)
+                    if adj.state is st.CellState.Hidden and not adj.mined:
+                        stack.appendleft(adj)
+    
+    def reveal_cell(self, loc: st.Location) -> bool:
+        """
+        Reveals the cell at the given location if it is hidden. Reveals all
+        other hidden cells connected to it if they are not mined.
+        
+        Attributes
+        ----------
+        loc : Location
+            the location of the cell reveal
+        
+        Returns
+        -------
+        mined : bool
+            If the revealed cell was mined
+        """
+        cell = self.select(loc)
+        if cell is None or cell.state is not st.CellState.Hidden or cell.mined:
+            return cell.mined
+        self.__inner_reveal(cell)
+               
+    def reveal_and_distribute(self, loc: st.Location, num: int) -> bool:
+        """
+        Reveals the cell at the given location if it is hidden and not mined. 
+        Distributes mines on other hidden cells on the board and reveals other 
+        hidden cells connected to the cell at the given location if they are 
+        not mined.
+        
+        Attributes
+        ----------
+        loc : Location
+            the location of the cell reveal
+        num : int
+            the number of mines to distribute
+        
+        Returns
+        -------
+        success : bool
+            If the cell at the given location could be revealed
+        """
+        cell = self.select(loc) 
+        if cell is None or cell.state is not st.CellState.Hidden or cell.mined:
+            return False
+        cell.set_state(st.CellState.Visible)
+        self.distribute_mines(num)
+        self.__inner_reveal(cell)
+        return True
+     
+    def distribute_mines(self, num: int):
+        """
+        Distributes up to the given number of mines on board cells randomly. 
+        
+        Attributes
+        ----------
+        num : int
+            the number of mines to distribute
+        """
+        cells = self.get_all_cells()
+        while num > 0 and len(cells) > 0:
+            i = randint(0, len(cells) - 1)
+            cell = cells[i]
+            if cell.state is st.CellState.Hidden and not cell.mined:
+                num -= 1
+                cell.set_mined()
+            cells.pop(i)
+    
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, Board) or self.size != other.size:
             return False
         for (row_a, row_b) in zip(self.rows, other.rows):
