51deae3e63d5520382f488c079217835179080c3
Jakob Wallén <57936645+JoXW100@users.noreply.github.com>
2022-11-22 16:31:00 +0100

    Implemented navigate action(s) (#19)

--- src/structure/actions/__init__.py
+++ src/structure/actions/__init__.py
@@ -1,3 +1,10 @@
 from .exit import Exit
+from .navigate import NavigateUp, NavigateRight, NavigateDown, NavigateLeft
 
-ALL_ACTIONS = [Exit()]
+ALL_ACTIONS = [
+    NavigateUp(), 
+    NavigateRight(), 
+    NavigateDown(), 
+    NavigateLeft(), 
+    Exit()
+]
