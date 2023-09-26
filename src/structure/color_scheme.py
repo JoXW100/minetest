8e3ecd72ece4b677fd4946621018941b139db335
Joel Sikström <joel.attano@gmail.com>
2023-09-26 19:34:39 +0200

    Correct singleton pattern refactoring.

--- src/structure/color_scheme.py
+++ src/structure/color_scheme.py
@@ -17,49 +17,55 @@ class ColorScheme:
     """
 
     DEFAULT_PRESET = 'default'
-    _instance = None
+    __instance = None
 
     def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(ColorScheme, cls).__new__(cls)
+        if cls.__instance is None:
+            cls.__instance = super(ColorScheme, cls).__new__(cls)
+            cls.__initialized = False
+        return cls.__instance
+    
+    def __init__(self):
+        if self.__initialized:
+            return
+        else:
+            self.__initialized = True
 
-            color_map = st.Color.color_map
-            cls._instance._presets = {
-                ColorScheme.DEFAULT_PRESET: {
-                    'flag': color_map["red"],
-                    'number_1': color_map["blue"],
-                    'number_2': color_map["green"],
-                    'number_3': color_map["red"],
-                    'number_4': color_map["navy"],
-                    'number_5': color_map["magenta"],
-                    'number_6': color_map["cyan"],
-                    'number_7': color_map["black"],
-                    'number_8': color_map["white"],
-                    'bomb': color_map["red"],
-                    'selected': color_map["green"],
-                },
-                'boring': {
-                    'flag': color_map["black"],
-                    'number_1': color_map["blue"],
-                    'number_2': color_map["blue"],
-                    'number_3': color_map["blue"],
-                    'number_4': color_map["blue"],
-                    'number_5': color_map["blue"],
-                    'number_6': color_map["blue"],
-                    'number_7': color_map["blue"],
-                    'number_8': color_map["blue"],
-                    'bomb': color_map["red"],
-                    'selected': color_map["green"],
-                },
-                'wild': {
-                    'flag': color_map["magenta"],
-                    'bomb': color_map["white"],
-                    'selected': color_map["magenta"],
-                }
+        color_map = st.Color.color_map
+        self.__presets = {
+            ColorScheme.DEFAULT_PRESET: {
+                'flag': color_map["red"],
+                'number_1': color_map["blue"],
+                'number_2': color_map["green"],
+                'number_3': color_map["red"],
+                'number_4': color_map["navy"],
+                'number_5': color_map["magenta"],
+                'number_6': color_map["cyan"],
+                'number_7': color_map["black"],
+                'number_8': color_map["white"],
+                'bomb': color_map["red"],
+                'selected': color_map["green"],
+            },
+            'boring': {
+                'flag': color_map["black"],
+                'number_1': color_map["blue"],
+                'number_2': color_map["blue"],
+                'number_3': color_map["blue"],
+                'number_4': color_map["blue"],
+                'number_5': color_map["blue"],
+                'number_6': color_map["blue"],
+                'number_7': color_map["blue"],
+                'number_8': color_map["blue"],
+                'bomb': color_map["red"],
+                'selected': color_map["green"],
+            },
+            'wild': {
+                'flag': color_map["magenta"],
+                'bomb': color_map["white"],
+                'selected': color_map["magenta"],
             }
-            cls._instance._current_scheme = ColorScheme.DEFAULT_PRESET
-
-        return cls._instance
+        }
+        self.__current_scheme = ColorScheme.DEFAULT_PRESET
 
     def set_color_scheme(self, scheme_name):
         """
@@ -72,17 +78,20 @@ class ColorScheme:
         scheme_name: str
             The name of the color scheme to set
         """
-        if scheme_name in self._presets:
-            self._current_scheme = scheme_name
+        if scheme_name in self.__presets:
+            self.__current_scheme = scheme_name
     
     def cycle_color_scheme(self):
         """
         Sets the current color scheme to the next one in the presets list.
         Useful for cycling color schemes in a menu.
         """
-        presets = list(self._presets.keys())
-        self._current_scheme = presets[(presets.index(
-            self._current_scheme) + 1) % len(presets)]
+        presets = list(self.__presets.keys())
+
+        # Calculate next idx (this loops around )
+        next_idx = (presets.index(self.__current_scheme) + 1) % len(presets)
+
+        self.__current_scheme = presets[next_idx]
 
     def get_current_scheme(self) -> str:
         """
@@ -93,7 +102,7 @@ class ColorScheme:
         scheme_name: str
             The name of the color scheme to set
         """
-        return self._current_scheme
+        return self.__current_scheme
 
     def get_color_schemes(self) -> [str]:
         """
@@ -104,7 +113,7 @@ class ColorScheme:
         color_schemes: [str]
             A list of all available color schemes.
         """
-        return self._presets.keys()
+        return self.__presets.keys()
 
     def get_color(self, element_name) -> str:
         """
@@ -123,11 +132,11 @@ class ColorScheme:
             the currently selected color scheme or the default color scheme.
         """
         # Lookup the color in the current preset.
-        preset_color = self._presets[self._current_scheme].get(element_name, None)
+        preset_color = self.__presets[self.__current_scheme].get(element_name, None)
 
         # If the color is defined in the current preset, return it.
         if preset_color is not None:
             return preset_color
 
         # Otherwise, try to get it from the default preset.
-        return self._presets[ColorScheme.DEFAULT_PRESET].get(element_name, None)
+        return self.__presets[ColorScheme.DEFAULT_PRESET].get(element_name, None)
